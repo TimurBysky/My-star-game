58f378b08726709136e4f7cda27c908b9186c0fd
Timur_Bysky <Timurbysky@gmail.com>
2026-01-16 16:03:27 +0300

    Старт

--- main_scene.gd
+++ main_scene.gd
@@ -1,12 +1,8 @@
 extends Node2D
 
-var warpNetwork: WarpNetwork
+var warpNetwork: GalaxyGeneration
 
 func _ready() -> void:
-	warpNetwork = WarpNetwork.new()
-	add_child(warpNetwork)
-	warpNetwork.generate_warps($Warps)
 
 
 func _on_generate_button_pressed() -> void:
-	warpNetwork.generate_warps($Warps)
