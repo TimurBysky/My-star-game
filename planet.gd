6f2df71e23f14a1175e74e7ec6efdec67caeba91
Timur <timurbysky@mail.ru>
2026-03-07 17:05:43 +0300

    Добавлено отображение параметров планеты

--- planet.gd
+++ planet.gd
@@ -2,7 +2,10 @@ extends Node3D
 @onready var animation: AnimationPlayer = $Planet_model/AnimationPlayer
 @onready var camera = $Camera3D
 @onready var UI = $UI_Layer
-
+ 
+@onready var science_label = $UI_Layer/UI/MarginContainer/VBoxContainer3/science_label
+@onready var food_label = $UI_Layer/UI/MarginContainer/VBoxContainer3/food_label
+@onready var industry_label = $UI_Layer/UI/MarginContainer/VBoxContainer3/industry_label
 var tween: Tween
 var before
 var after
@@ -11,6 +14,14 @@ var current_layer: int
 var galaxy_camera: Camera3D
 var galaxy_UI: CanvasLayer
 
+signal update_status
+
+var performance = \
+{
+	"science" :0,
+	"food" : 0,
+	"industry" : 0
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,7 +36,7 @@ func _ready() -> void:
 			if not child is Area3D and not child is Camera3D:
 				child.layers = current_layer  # Правильно для 3D!
 				print(current_layer)
-
+	update_status.connect(show_status)
 	camera.cull_mask = current_layer
 
 func planet_in():
@@ -51,6 +62,7 @@ func activate_UI(_galaxy_camera: Camera3D, _galaxy_UI: CanvasLayer):
 	galaxy_UI.visible = false
 	UI.visible = true
 	camera.current = true
+	emit_signal("update_status")
 	
 func diactivate_UI():
 	galaxy_camera.current = true
@@ -58,6 +70,15 @@ func diactivate_UI():
 	camera.current = false
 	UI.visible = false
 
-
+func show_status():
+	var science_value = performance.get("science") # или performance["science"]
+	var food_value = performance.get("food")
+	var industry_value = performance.get("industry")
+	
+	science_label.text = "Уровень науки: " + str(science_value)
+	food_label.text =  "Уровень еды: " + str(food_value)
+	industry_label.text =  "Уровень промышленности: " + str(industry_value)
+	print("status")
+	
 func _on_exit_button_pressed() -> void:
 	diactivate_UI()
